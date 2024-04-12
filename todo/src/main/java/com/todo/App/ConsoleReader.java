36c0d09ffc00a1f791c14888ecce45ec70c2a449
Rishav <140265067+IamRishavDas@users.noreply.github.com>
2024-04-13 00:40:18 +0530

    made the Welcome.txt file as realtive path using URI and the added jar file but the Store.dat is still absolute to the path

--- todo/src/main/java/com/todo/App/ConsoleReader.java
+++ todo/src/main/java/com/todo/App/ConsoleReader.java
@@ -1,44 +1,53 @@
 package com.todo.App;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import com.todo.PatternMatcher.CommandPatternMatcher;
 import com.todo.TaskOperations.TaskManagement;
 
 public class ConsoleReader {
-    public ConsoleReader() throws IOException{
+    public ConsoleReader() throws IOException {
         boolean exit = false;
         showWelcomeScreen();
         // TaskManagement.loadTasks();
         while (!exit) {
             String command = System.console().readLine(":");
             switch (CommandPatternMatcher.getCommandString(command)) {
-                case "add"     -> TaskManagement.addTask(command);
-                case "show"    -> TaskManagement.showTasks(command);
-                case "mark"    -> TaskManagement.markTask(command);
-                case "remove"  -> TaskManagement.removeTask(command);
-                case "save"    -> TaskManagement.saveTasks();
-                case "load"    -> TaskManagement.loadTasks();
-                case "delete"  -> TaskManagement.deleteSavedTasks();
-                case "clear"   -> TaskManagement.clearTerminal();
-                case "exit"    -> exit = true;         
-                default        -> System.out.println("NOT SUITABLE COMMAND FOUND!!");
+                case "add" -> TaskManagement.addTask(command);
+                case "show" -> TaskManagement.showTasks(command);
+                case "mark" -> TaskManagement.markTask(command);
+                case "remove" -> TaskManagement.removeTask(command);
+                case "save" -> TaskManagement.saveTasks();
+                case "load" -> TaskManagement.loadTasks();
+                case "delete" -> TaskManagement.deleteSavedTasks();
+                case "clear" -> TaskManagement.clearTerminal();
+                case "exit" -> exit = true;
+                default -> System.out.println("NOT SUITABLE COMMAND FOUND!!");
             }
         }
     }
-    public void showWelcomeScreen() throws FileNotFoundException, IOException{
 
-        final String filePath = System.getProperty("user.dir") + File.separator + "todo" + File.separator + "src" + File.separator + "main" + File.separator +"java" + File.separator + "com" + File.separator + "todo"  + File.separator + "UtilityFiles" + File.separator + "Welcome.txt";
+    public void showWelcomeScreen() throws FileNotFoundException, IOException {
 
-        File welcomeFile = new File(filePath);
+        ClassLoader classLoader = ConsoleReader.class.getClassLoader();
 
-        Scanner scanner = new Scanner(welcomeFile);
-        while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+        try (InputStream inputStream = classLoader.getResourceAsStream("Welcome.txt")) {
+            if (inputStream != null) {
+                Scanner scanner = new Scanner(inputStream);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                }
+                scanner.close();
+            } else {
+                System.out.println("ERROR: WELCOME CONTENT IS MISSING!!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        scanner.close();
+
     }
 }
